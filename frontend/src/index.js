4efccfb9bb890f51fa7235643419410bb530a904
Julia Finarovsky <juliafinarovsky@gmail.com>
2019-02-06 21:00:01 -0500

    Added polyfills for older browsers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
+// Polyfills for older browsers
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'raf/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import store from './store';
 import {Router} from 'react-router-dom';
 import history from './history';
+import './index.css';
 
 
 ReactDOM.render(
